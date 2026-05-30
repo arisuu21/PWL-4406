3dd700ae405d564a05d25445bf24f317775815e1
Andra <andradt21@gmail.com>
2026-05-30 21:47:49 +0700

    Add create update delete product feature

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,12 +5,21 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-
 $routes->get('/', 'Home::index', ['filter' => 'auth']);
 
 $routes->get('login', 'AuthController::login');
-$routes->post('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login', ['filter' => 'redirect']);
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']);
+$routes->group('produk', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'ProdukController::index');
+    $routes->post('', 'ProdukController::create');
+    $routes->post('edit/(:any)', 'ProdukController::edit/$1');
+    $routes->get('delete/(:any)', 'ProdukController::delete/$1');
+});
+
 $routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']);
+
+$routes->get('faq', 'Home::faq', ['filter' => 'auth']);
+$routes->get('profile', 'Home::profile', ['filter' => 'auth']);
+$routes->get('contact', 'Home::contact', ['filter' => 'auth']);
